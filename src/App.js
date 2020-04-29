96ff851a4a02ce266769f0c2988f211e5a0d7d47
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-29 13:41:49 +0700

    Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Cards from './components/Cards'
+import Forms from './components/Forms'
 import generateMain from "./hoc/generateMain";
 const CardWithLayout = generateMain(Cards);
+const FormsWithLayout = generateMain(Forms);
 function App() {
   return (
     <div>
-          <CardWithLayout />
+          <FormsWithLayout />
     </div>
 
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';  
 
 const Form = () => {
-    return ( <div>sdsadassss</div>);
+    return ( <div></div>);
 }
  
 export default Form;
