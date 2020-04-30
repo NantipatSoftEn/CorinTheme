98a21964211a6df3815911cc68e0acd6bba164f0
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-30 21:35:53 +0700

    remove Header form HOC

--- src/views/layout/Main.js
+++ src/views/layout/Main.js
@@ -9,9 +9,6 @@ const Main = (props) => {
       <div class="flex flex-col md:flex-row">
         <Navbar />
         <div class="main-content flex-1 bg-gray-300 mt-12 md:mt-2 pb-24 md:pb-5">
-          <div class="bg-blue-800 p-2 shadow text-xl text-white">
-            <h3 class="pl-2">Analytics</h3>
-          </div>
             {props.children}
         </div>
       </div>
