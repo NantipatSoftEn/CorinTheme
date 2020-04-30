8144a17b0eccb41dac32eba76600e98cd523b829
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-30 23:32:13 +0700

    Add take

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 import Main from "./views/layout/Main";
 import Cards from "./components/Cards";
 import Forms from "./components/Forms";
+import Table from "./components/Table";
 import generateMain from "./hoc/generateMain";
 const history = createBrowserHistory();
 const CardWithLayout = generateMain(Cards);
 const FormsWithLayout = generateMain(Forms);
+const TableWithLayout = generateMain(Table);
 function App() {
   return (
     <div>
@@ -16,6 +18,7 @@ function App() {
       <Route exact path="/" component={Main} />
         <Route exact path="/form" component={FormsWithLayout} />
         <Route exact path="/card" component={CardWithLayout} />
+        <Route exact path="/table" component={TableWithLayout} />
       </Router>
     </div>
   );
